dced20eccca7baa3a19ef070bddb76b25345c8af
Scott Chamberlin <scottcha@live.com>
2011-05-08 06:03:52 -0700

    Initial checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coop::Application.config.secret_token = '5b8dbcd7d6b2c2e768f1931e5a0b5c2fdb240045192ae8c8c2095ac3f362bcfd3bb1d7069713bef6c9ef1aeee44dd6b3ae2a709fd6ba4a707e0e04d198b7a848'
